4535b0de33f7df6bc8c29adaaa65534d586eb637
Diekkan <merentiel.diego@gmail.com>
2022-01-13 14:40:33 -0800

    pchar added

--- _pchar.c
+++ _pchar.c
@@ -11,7 +11,7 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L<%d>: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (((*stack)->n < 65 || (*stack)->n > 90) && ((*stack)->n < 97 || (*stack)->n > 122))
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L<%d>: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
