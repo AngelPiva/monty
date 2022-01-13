8d9beecc24556c72cc761535b12c9369e52e4932
Diekkan <merentiel.diego@gmail.com>
2022-01-13 13:15:13 -0800

    some issues fixed

--- 00.m
+++ 00.m
@@ -1,7 +1,10 @@
-                       	
-push 25
-push		 25
-
-	pall
-add
+push 60
+push 20
+push 30
+pall
+pop
+pall
+pop
+pall
+      pop
 pall
--- add_node_int.c
+++ add_node_int.c
@@ -14,7 +14,10 @@ stack_t *add_nodeint(stack_t **head, const int n)
 
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
-		return (NULL);
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
 	new->n = n;
 	if (tmp == NULL)
 	{
