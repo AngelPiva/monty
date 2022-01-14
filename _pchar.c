c1d5871031699c9a736cf73b603629c0c8d10736
AngelPiva <angelpiva6@gmail.com>
2022-01-13 16:52:47 -0800

    fixed betty

--- _pchar.c
+++ _pchar.c
@@ -6,7 +6,7 @@
  */
 void _pchar(stack_t **stack, unsigned int line_number)
 {
-	if(!*stack)
+	if (!*stack)
 	{
 		fprintf(stderr, "L<%d>: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
