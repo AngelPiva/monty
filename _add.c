04e101b549394b1c007b7a6938e0490f2d88bbf2
AngelPiva <angelpiva6@gmail.com>
2022-01-13 17:38:54 -0800

    add

--- _add.c
+++ _add.c
@@ -13,7 +13,7 @@ void _add(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || (*stack)->next)
 	{
-		fprintf(stderr, "L<%d>: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	num1 = (*stack)->n;
