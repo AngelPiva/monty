80d3fcd113b2cab36a8219261c2a75e789487995
AngelPiva <angelpiva6@gmail.com>
2022-01-13 17:49:19 -0800

    mod

--- _mod.c
+++ _mod.c
@@ -13,12 +13,12 @@ void _mod(stack_t **stack, unsigned int line_number)
 
 	if (num1 == 0)
 	{
-		fprintf(stderr, "L<%d>: division by zero\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if (!*stack || (*stack)->next)
 	{
-		fprintf(stderr, "L<%d>: can't mod, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	num1 = (*stack)->n;
